31b4ce9ed1077ad5805ffa86fc22bf82f174060d
Yurii <yural2home@gmail.com>
2024-11-18 18:09:50 +0200

    Task 4.1

---
+++ HillelTask_4.1/HillelTask_4.1/Program.cs
@@ -0,0 +1,23 @@
+﻿int inputUser;
+bool Cycle = true;
+
+while (Cycle)
+{
+    Console.WriteLine("Write number day week: ");
+    inputUser = Convert.ToInt16(Console.ReadLine());
+
+    string dayName = inputUser switch
+    {
+        1 => "Monday",
+        2 => "Tuesday",
+        3 => "Wednesday",
+        4 => "Thursday  ",
+        5 => "Friday ",
+        6 => "Saturday ",
+        7 => "Sunday ",
+        _ => "Unknown day"
+};
+
+    Console.WriteLine(dayName);
+
+}
