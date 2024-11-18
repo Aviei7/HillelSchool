e2df4fdfdd519dedbe465cf453416b74dff469b8
Yurii <yural2home@gmail.com>
2024-11-18 18:23:19 +0200

    Task 4.2

---
+++ HillelTask_4.2/HillelTask_4.2/Program.cs
@@ -0,0 +1,31 @@
+﻿int oneValue;
+int twoValue;
+char operationValue;
+int result = 0;
+
+Console.WriteLine("Input first value: ");
+oneValue = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input operation(*, /, -, +) value: ");
+operationValue = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("Input two value: ");
+twoValue = Convert.ToInt32(Console.ReadLine());
+
+switch (operationValue)
+{
+    case '-':
+        result = oneValue - twoValue; break;
+    case '+':
+        result = twoValue + oneValue; break;
+    case '*':
+        result = oneValue * twoValue; break;
+    case '/':
+        result = twoValue / oneValue; break;
+    default: Console.WriteLine("Operatot '" + operationValue + "' not exists"); break;
+};
+
+if (result != 0)
+{
+    Console.WriteLine("Result: " + result);
+};
