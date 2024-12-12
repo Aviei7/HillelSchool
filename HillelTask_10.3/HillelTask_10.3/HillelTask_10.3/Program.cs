b5b71f2e2712b2b38542a457b702dac31bf7fbe2
Yurii <yural2home@gmail.com>
2024-12-12 16:12:21 +0200

    Task_10.3

---
+++ HillelTask_10.3/HillelTask_10.3/HillelTask_10.3/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        string input;
+        int spaceIndex;
+
+        Console.WriteLine("Введіть ім'я та призвіще");
+        input = Console.ReadLine();
+
+        foreach (char c in input)
+        {
+            spaceIndex = input.IndexOf(' ');
+            if (spaceIndex != -1)
+            {
+                input = input.Remove(spaceIndex, 1);
+            }
+            else
+            {
+                break;
+            }
+        }
+        Console.WriteLine(input);
+    }
+}
