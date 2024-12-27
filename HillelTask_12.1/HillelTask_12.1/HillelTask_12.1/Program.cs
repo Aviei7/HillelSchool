8de25415e7ac67dea00b44c678e7f9bd1cbcda08
Yurii <yural2home@gmail.com>
2024-12-27 11:40:46 +0200

    Task_12.1

---
+++ HillelTask_12.1/HillelTask_12.1/HillelTask_12.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        int inputNumberCount;
+        Console.WriteLine($"Write count number: ");
+        inputNumberCount = int.Parse(Console.ReadLine());
+        int oneValue = 0;
+        int twoValue = 1;
+        int count = 0;
+        StringBuilder strB = new StringBuilder();
+        Recursion();
+
+        void Recursion()
+        {
+            oneValue += twoValue;
+            twoValue += oneValue;
+            if (oneValue < inputNumberCount)
+            {
+                strB.Append($"{oneValue} \n");
+            }
+            else
+            {
+                return;
+            }
+
+            if (twoValue < inputNumberCount)
+            {
+                strB.Append($"{twoValue} \n");
+            }
+            else
+            {
+                return;
+            }
+            count++;
+            Recursion();
+        }
+
+        Console.WriteLine($"Порядок фібоначчі \n{strB}");
+    }
+}
