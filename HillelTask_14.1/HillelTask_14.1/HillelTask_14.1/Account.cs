bff802cf4989610e2f1347647d4bcfe4d5051e66
Yurii <yural2home@gmail.com>
2025-01-01 23:20:19 +0200

    Task_14.1

---
+++ HillelTask_14.1/HillelTask_14.1/HillelTask_14.1/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HillelTask_14._1
+{
+    class Account
+    {
+        private string name;
+        private int balance { get; set; }
+
+        public int Balance
+        {
+            get { return balance;  }
+        }
+
+        public Account(string name, int balance)
+        {
+            this.name = name;
+            this.balance = balance;
+        }
+
+
+        public void Deposit(int deposit)
+        {
+            balance += deposit;
+        }
+
+        public void Withdrawal(int withDrawalValue)
+        {
+            balance -= withDrawalValue;
+        }
+    }
+}
---
+++ HillelTask_14.1/HillelTask_14.1/HillelTask_14.1/Program.cs
@@ -0,0 +1,8 @@
+﻿using HillelTask_14._1;
+
+Account yuriiAccount = new Account("Yurii account", 200);
+
+yuriiAccount.Deposit(100);
+Console.WriteLine("Balance Yurii is now: " + yuriiAccount.Balance);
+yuriiAccount.Withdrawal(120);
+Console.WriteLine("Balance Yurii is now: " + yuriiAccount.Balance);
